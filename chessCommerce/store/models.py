c68c3053cf92528b0f8b4b199c072e13f4bb965e
Nabin Oli <olinabin2004@gmail.com>
2024-09-29 05:05:28 +0545

    Added the products to the page

--- chessCommerce/store/models.py
+++ chessCommerce/store/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name 
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Customer(models.Model):
