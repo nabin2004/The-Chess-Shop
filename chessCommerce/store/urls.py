75ed1e44e603a42cc14e45734ca21b778dc0a5e3
Nabin Oli <olinabin2004@gmail.com>
2024-09-30 16:47:57 +0545

    Added the login and logout ppage

--- chessCommerce/store/urls.py
+++ chessCommerce/store/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('about/', views.about, name="about"),
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.login_user, name="logout"),
 ]
